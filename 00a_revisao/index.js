53575f5fe507721ade54f0e3a64ec61c496c5cc6
AnaSantos80 <aninha_rsantos@yahoo.com.br>
2019-10-28 13:25:20 -0300

    fiz os exercícios a e b

--- 00a_revisao/index.js
+++ 00a_revisao/index.js
@@ -1,7 +1,65 @@
 // 1.
-// [] adicionar a tag <script> no html
-// [] verificar se está lendo index.js
-// [] alterar cor de <body>
-// [] escutar o clique do botão
-// [] criar função de alterar a cor usando rgb()
-// [] usar Math.random() e Math.floor() ou parseInt()
+// [x] adicionar a tag <script> no html
+// [x] verificar se está lendo index.js
+// [x] alterar cor de <body>
+// [x] escutar o clique do botão
+// [x] criar função de alterar a cor usando rgb()
+// [x] usar Math.random() e Math.floor() ou parseInt()
+
+// console.log('olar');
+// Ctrl + X para eliminar a linha em qualquer parte, não há necessidade de selecionar
+
+// const body = document.querySelector('body');
+
+// const body = document.getElementsByTagName('body');
+
+
+const body = document.body
+// o "document" é um objeto que tem um body como filha
+// console.log(body);
+
+body.style.backgroundColor = 'purple';
+
+const btn = document.getElementById('trocaCor');
+// console.log(botao)
+
+body.style.backgroundColor = 'purple';
+// 'rgb(  ,  ,  )'
+
+// const n = Math.random();
+// const n2 = n * 255;
+// const int = parseInt(n2);
+
+// console.log('número:', n);
+// console.log('número2:', n2);
+// console.log('int:', int);
+
+
+// console.log(intRandom)
+
+btn.addEventListener('click', mudarCor);
+
+function geraInteiro() {
+    const n = parseInt(Math.random() * 255);
+    console.log('chamou geraInteiro',n);
+    return n;    
+}
+
+// F2 - muda o nome da const
+
+
+function mudarCor() {
+    // const intRandom = parseInt(Math.random() * 255)
+    const r = geraInteiro();
+    const g = geraInteiro();
+    const b = geraInteiro();
+
+    // console.log('entrei na função');
+    // body.style.backgroundColor = 'rgb('+intRandom+','+intRandom+','+intRandom+')'
+    body.style.backgroundColor = 'rgb(' + r +',' + g +',' + b +')'
+    console.log(' + r +',' + g +',' + b +');
+    console.log(r, g, b)
+}
+
+
+
