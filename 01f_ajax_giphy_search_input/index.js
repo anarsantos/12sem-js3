9b1492813f2e6d468e689812c349748df3fc36a5
AnaSantos80 <aninha_rsantos@yahoo.com.br>
2019-11-06 17:16:46 -0300

    atualizei os arquivos 01f e 04b

--- 01f_ajax_giphy_search_input/index.js
+++ 01f_ajax_giphy_search_input/index.js
@@ -1,3 +1,89 @@
 // XMLHttpRequest na rota search capturando informação do input
 // trazer 10 imagens
 // apresentar as imagens na div #root
+
+const input = document.querySelector('#gifInput');
+// console.log(input);
+
+const botao = document.querySelector('#gifButton');
+// console.log(botao);
+
+const div = document.querySelector('#gifInput')
+// console.log(div);
+
+botao.addEventListener('click', function (evento) {
+    fazerAlgoDepoisDeClicar(evento)
+});
+
+function fazerAlgoDepoisDeClicar(evento) {
+    fazerAlgoDepoisDeClicar(evento)
+    
+});
+
+function fazerAlgoDepoisDeClicar(evento) {
+    evento.preventDefault();
+    div.innerHTML.innerHTML = '';
+    // console.log('será que eu cliquei??'
+    const vakirDoInput = input.nodeValue;
+    input.value = '';
+    // console.log(valorDoInput)
+
+    const minhaChave = 'dc6zaTOxFJmzC'
+    const oQueQueroBuscar = valorDoInput;
+    const endercoAPI = `http://api.giphy.com/v1/gifs/search?api_key=${minhaChave}&q=${oQueQueroBuscar}&limit=3`
+    // console.log(endercoAPI)
+
+
+    
+    
+}
+
+    const valorDoInput = input.nodeValue;
+    // console.log(valorDoInput)
+
+
+    console.log(enderecoAPI);
+
+    const requisicaoHttp = new XMLHttpRequest()
+    requisicaoHttp.open('GET', endercoAPI);
+    requisicaoHttp.send();
+    requisicaoHttp.onreadystatechange = function pegandoResposta() {
+        if (requisicaoHttp.readyState === 4) {
+            if (requisicaoHttp.readyState === 200) {
+                console.log('a resposta esá pronta')
+            }
+
+            else {
+                const statusCode = requisicaoHttp.status;
+                const statusMensagem = requisicaoHttp.statusText;
+                // console.log('Este é meu console log', statusCode, statusMensagem);
+                console.error('Este é o meu console error', statusCode, statusMensagem);
+                console.warn('Este é meu console warn');
+
+                div.innerHTML = `<h1>Falha ${statusCode}: ${statusMensagem}</h1>`
+
+                // console.log('meu, deu ruim na requisição e não tem resposta', requisicaoHttp.status);
+
+            }
+
+            const resposta = requisicaoHttp.response;
+            const respTransfObj = JSON.parse(resposta);
+            console.log(respTransfObj);
+            // console.log(respTransfObj.data);
+            const arrData = respTransfObj.data;
+            console.arrData(arrData); 
+
+
+        } else {
+
+            // console.log('a resposta não está pronta');
+
+        }
+
+    }
+}
+
+// Pesquisar depois sobre "console error"
+
+
+
--- 04b_fetch_giphy_search/index.js
+++ 04b_fetch_giphy_search/index.js
@@ -5,3 +5,42 @@
 // [] usar o método fetch() para a url da API Giphy
 // [] transformar o dado recebido
 // [] apresentar <img>
+
+const input = document.querySelector('#gifInput');
+const botao = document.querySelector('#gifButton');
+const div = document.querySelector('#root')
+
+botao.addEventListener('click', (evento) => fazerFetch(evento));
+
+function fazerFetch(evento) {
+    evento.preventDefault();
+    const valorDoInput = input.value;
+    // console.log(valorDoInput);
+
+    div.innerHTML = ''    
+    const url = `http://api.giphy.com/v1/gifs/search?q=${valorDoInput}&api_key=dc6zaTOxFJmzC&limit=4offset=0`
+
+    fetch(url)
+        .then(resposta => {
+            if (!resposta.ok) {
+            console.error(resposta.statusText);
+            mostrarErro(resposta.statusText);
+        }
+        return resposta.json()
+    })
+
+        .then(json => mostrarResultado(json))
+        .catch(erro => console.error(erro))
+}
+
+function mostrarResultado(json) {
+    const arr = json.data
+    console.log(arr);
+    arr.forEach(element => {
+        const scrImg = element.images.original.url;
+        div.innerHTML += `
+        <img src=${scrImg}>
+        `
+    });
+}
+
